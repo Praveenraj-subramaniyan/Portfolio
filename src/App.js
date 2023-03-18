0604060c98b857c4d4578895ea254d8a9d323bbd
spr887011 <spr887011@gmail.com>
2023-03-18 20:14:21 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -4,26 +4,26 @@ import './App.css';
 function App() {
   return (
     <div class="body">
-    <div class="header">
-    <div class="header-right">
-      {/* <img  src="C:/Visual Studio Code/portfolio/homepage-icon-png-2598 (1).png" alt='Home'/> */}
+     <div class="header">
+      <div class="header-right">
+      {/* <img  src="../public/favicon.ico" alt='Home'/> */}
       <a href="#home">Home</a>
       <a href="#experience">Experience</a>
       <a href="#education">Education</a>
       <a href="#contact">Contact</a>
+      </div>
+     </div>
+    <div className='home' id='contachomet'>
+       <p>----------</p>
     </div>
+    <div className='experience' id='experience'>
+    <p>----------</p>
     </div>
-    <div className='home'>
-
+    <div className='education' id='education'>
+    <p>----------</p>
     </div>
-    <div className='experience'>
-
-    </div>
-    <div className='education'>
-
-    </div>
-    <div className='contact'>
-
+    <div className='contact' id='contact'>
+    <p>----------</p>
     </div>
   </div>
   );
