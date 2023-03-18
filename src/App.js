bc06a5722b798065ab2d03d21ff14670d9af981e
spr887011 <spr887011@gmail.com>
2023-03-18 17:48:54 +0530

    image added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import './App.css';
 
 function App() {
   return (
+    <div class="body">
     <div class="header">
     <div class="header-right">
+      {/* <img  src="C:/Visual Studio Code/portfolio/homepage-icon-png-2598 (1).png" alt='Home'/> */}
       <a href="#home">Home</a>
-      <a href="#about">Experience</a>
-      <a href="#about">Education</a>
+      <a href="#experience">Experience</a>
+      <a href="#education">Education</a>
       <a href="#contact">Contact</a>
     </div>
+    </div>
     <div className='home'>
+
+    </div>
+    <div className='experience'>
+
+    </div>
+    <div className='education'>
+
+    </div>
+    <div className='contact'>
 
     </div>
   </div>
