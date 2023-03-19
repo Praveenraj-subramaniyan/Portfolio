42404eb71f1ea026ae8ac9c692fcd0d5dee63940
spr887011 <spr887011@gmail.com>
2023-03-19 15:17:21 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div >
@@ -27,22 +29,24 @@ function App() {
           </div>
         </div>
       </nav>
-      <div class="home mx-auto" id='home'>
-        <div class="row">
-          <div class="col-sm-4">.col-sm-4</div>
+      <div class="home mx-auto " id='home'>
+        <div class="row content">
+          <div class="col-sm-4">
+          <img src='https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/havjfrq3liewx4q6eitx' alt='Profile' />
+          </div>
           <div class="col-sm-8">
             <h3>Praveenraj Subramaniyan | Certified full stack developer</h3>
             <p>To start in making a professional full stack developer resume,let's clarify the distinction between a CV and a resume. Although it may sound tricky, there are actually evident features that enable us to differentiate. The features are namely its Content, Objective, and Length.</p>
           </div>
         </div>
       </div>
-      <div class="experience" id='experience'>
+      <div class="experience mx-auto content" id='experience'>
         <p>experience</p>
       </div>
-      <div class="education" id='education'>
+      <div class="education mx-auto content" id='education'>
         <p>education</p>
       </div>
-      <div class="contact" id='contact'>
+      <div class="contact mx-auto content" id='contact'>
         <p>contact</p>
       </div>
     </div>
